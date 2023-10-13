5b8b7ceb161a1b9ed6305263085acd9776e57900
iam5abhi <support@codesoftic.com>
2023-10-13 12:19:04 +0530

    main

--- src/Config/BaseUrl.js
+++ src/Config/BaseUrl.js
@@ -1,2 +1,2 @@
-export default  {url:"http://localhost:7000"}
-// export default  {url:"https://neoxs.herokuapp.com"}
+// export default  {url:"http://localhost:7000"}
+export default  {url:"https://my-talent-cabin-backend.onrender.com"}
